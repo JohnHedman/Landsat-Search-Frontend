4e9e6de31c92d66168d80f0522ac86c93c36cbfe
jdholst <jacob.holst@jacks.sdstate.edu>
2019-05-03 13:16:19 -0500

    Added save to favorites feature

--- src/app/listgrid/listgrid.component.ts
+++ src/app/listgrid/listgrid.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { SceneItem } from '../scene-item/scene-item.model';
 
+import { HttpService } from '../scene-catalog/http.service';
+import { UserService } from '../google-signin/user.service';
+
 @Component({
   selector: 'app-listgrid',
   templateUrl: './listgrid.component.html',
@@ -9,10 +12,14 @@ import { SceneItem } from '../scene-item/scene-item.model';
 export class ListgridComponent implements OnInit {
   @Input() scene: SceneItem;
 
-  constructor() { }
+  constructor(private http: HttpService, private userService: UserService) { }
 
   ngOnInit() {
     console.log(this.scene);
   }
-  
+
+  onSaveScene() {
+    this.http.addFavorites(`?User=${this.userService.currentUser.name}&Scene=${this.scene.id}`);
+  }
+
 }
