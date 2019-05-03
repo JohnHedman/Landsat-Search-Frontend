dbaa30f64bc01bd7ff7b01770a436baab7cde7a2
jdholst <jacob.holst@jacks.sdstate.edu>
2019-05-02 19:51:18 -0500

    Added form controls for search input

--- src/app/search-input/search-input.component.ts
+++ src/app/search-input/search-input.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search-input',
@@ -7,5 +8,20 @@ import { Component } from '@angular/core';
 })
 
 export class SearchInputComponent {
-  value = 'Latitude';
+  @Output() search = new EventEmitter<any>();
+
+  searchForm = new FormGroup({
+    longitude: new FormControl(-96, Validators.required),
+    latitude: new FormControl(44, Validators.required),
+    cloud_cover: new FormControl(),
+    sun_elevation: new FormControl(),
+    start_date: new FormControl(),
+    end_date: new FormControl()
+  });
+
+  onSearch() {
+    if (this.searchForm.valid) {
+      this.search.emit(this.searchForm.value);
+    }
+  }
 }
