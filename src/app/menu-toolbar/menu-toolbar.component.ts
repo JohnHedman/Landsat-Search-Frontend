f814482cba339e6f482a72096054b2c1ffb7f3d9
jdholst <jacob.holst@jacks.sdstate.edu>
2019-05-03 00:21:14 -0500

    Added user select

--- src/app/menu-toolbar/menu-toolbar.component.ts
+++ src/app/menu-toolbar/menu-toolbar.component.ts
@@ -13,7 +13,14 @@ export interface User {
 export class MenuToolbarComponent implements OnInit {
   userNames: User[] = [
     { name: 'John' },
+    { name: 'Hamer' },
+    { name: 'Werpy' },
+    { name: 'Jacob' },
+    { name: 'Jamison' },
+    { name: 'Gamradt' }
   ];
+
+  currentUser: User = this.userNames[0];
 
   constructor() { }
 
