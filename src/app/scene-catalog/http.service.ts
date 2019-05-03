1fc439799834335f5b9c58348de199846e01aff9
JohnHedman <Jonathan.Hedman@sdstate.edu>
2019-05-03 00:10:34 -0500

    Added more HTTP stuff

--- src/app/scene-catalog/http.service.ts
+++ src/app/scene-catalog/http.service.ts
@@ -3,12 +3,30 @@ import { Headers, Http, Response } from '@angular/http';
 
 @Injectable()
 export class HttpService {
-    apigatewayendpoint = 'https://1yaf2o5ra7.execute-api.us-west-2.amazonaws.com/Production/search';
+    searchEndpoint = 'https://1yaf2o5ra7.execute-api.us-west-2.amazonaws.com/Production/search';
+    getFavoritesEndpoint = 'https://1yaf2o5ra7.execute-api.us-west-2.amazonaws.com/Production/check-user-favs';
+    addFavoritesEndpoint = 'https://1yaf2o5ra7.execute-api.us-west-2.amazonaws.com/Production/add-scene';
+    getSceneEndpoint = 'https://1yaf2o5ra7.execute-api.us-west-2.amazonaws.com/Production/item';
 
     constructor(private http: Http) {}
 
     getItems(body: string) {
         const headersOpt = new Headers({'Content-Type': 'application/json'});
-        return this.http.get(this.apigatewayendpoint + body, {headers: headersOpt});
+        return this.http.get(this.searchEndpoint + body, {headers: headersOpt});
+    }
+
+    getScene(body: string) {
+        const headersOpt = new Headers({'Content-Type': 'application/json'});
+        return this.http.get(this.getSceneEndpoint + body, {headers: headersOpt})
+    }
+
+    getFavorites(body: string) {
+        const headersOpt = new Headers({'Content-Type': 'application/json'});
+        return this.http.get(this.getFavoritesEndpoint + body, {headers: headersOpt})
+    }
+
+    addFavorites(body: string) {
+        const headersOpt = new Headers({'Content-Type': 'application/json'});
+        return this.http.post(this.addFavoritesEndpoint + body, {headers: headersOpt})
     }
 }
